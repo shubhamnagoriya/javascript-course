7d8b0ad79c6334559464987a3ba39d0503831163
Shubham Nagoriya <shubham34nagori@gmail.com>
2025-09-15 19:04:26 +0000

    Add variables and datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat this js code as newer version Js code
+
+alert() // we're using Node.js not browser
+
+// Code readability is very important, keep your code structured and easy to read
+
+// Documentations to understand theory or standards
+// MDN documentations by Mozilla
+// tc39.es documentation
+
+
+// datatypes in Javascript
+// String: store text data
+// Integer: store number data
+// boolean: store true/fales about the case or situation
+// Null: empty space or containing nothing
+// undefined: value is not defined yet
+// symbols: uses symbols
+// object: 
