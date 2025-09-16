fcd8e39f6eab37b754ece97c3cc1f572148780e3
Shubham Nagoriya <shubham34nagori@gmail.com>
2025-09-16 07:28:50 +0000

    datatypes explain

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,6 +1,6 @@
 "use strict" // treat this js code as newer version Js code
 
-alert() // we're using Node.js not browser
+// alert() // we're using Node.js not browser
 
 // Code readability is very important, keep your code structured and easy to read
 
@@ -10,10 +10,25 @@ alert() // we're using Node.js not browser
 
 
 // datatypes in Javascript
+
 // String: store text data
-// Integer: store number data
+console.log("This is a string value");
+
+// Integer: store number 
+console.log(43453234);
+
 // boolean: store true/fales about the case or situation
+let userActive = true
+console.log(userActive);
+
 // Null: empty space or containing nothing
+console.log(null);
+
 // undefined: value is not defined yet
+console.log(undefined);
+
 // symbols: uses symbols
+
+// Note: "null" datatype is object
+
 // object: 
